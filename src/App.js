4d8b4cd85f32379cee24469de0779b8f95eed7f2
Abavish <vnand@csu.fullerton.edu>
2018-09-19 21:07:39 -0700

    app component with state

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        
-      </div>
-    );
-  }
+  constructor(props){
+    super(props);
+
+    this.state = {
+      userDetails: [
+      {
+        userName: 'ViSHaL NanD',
+        userAccount: 'abavish',
+        postDate: 'Sept 13',
+      },
+      {
+        userName: 'Abby NanD',
+        userAccount: 'djabhi',
+        postDate: 'Sept 18',
+      }
+      ]
+    }
+  }	
+	  render() {
+	    return (
+	      <div>
+	        <h1>Social Card Demo</h1>
+	        <div className="socialCard">
+	          <ProfilePic userDetails={this.state.userDetails} />
+	        </div>
+	      </div>
+	    );
+	  }
 }
 
 export default App;
